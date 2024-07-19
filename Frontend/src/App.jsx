781bb55dcfc8db8ed702e44a0587b4e67281a061
rashi <rashichourasiya9026@gmail.com>
2024-07-20 00:34:44 +0530

    add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 import Fontpage from './pages/Fontpage';
+import Sidebar from './pages/Sidebar';
 
 function App() {
   return (
 
     <>
         <Fontpage />
-    
+        <Sidebar></Sidebar>
+
     </>
   );
 }
